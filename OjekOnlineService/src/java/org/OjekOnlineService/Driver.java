cf209eec4e7e246df22cdc5c29c9e69eaad70835
adyanf <adyanaufalf@gmail.com>
2017-11-07 19:36:58 +0700

    edit driver models

--- OjekOnlineService/src/java/org/OjekOnlineService/Driver.java
+++ OjekOnlineService/src/java/org/OjekOnlineService/Driver.java
@@ -5,6 +5,7 @@
  */
 package org.OjekOnlineService;
 
+import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -13,7 +14,13 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author adyan
  */
 @XmlRootElement(name = "Driver")
-public class Driver extends User {
+public class Driver {
+    @XmlElement(name = "ID", required = true)
+    private int id;
+    
+    @XmlElement(name = "name", required = true)
+    private String name;
+    
     @XmlElement(name = "total_rating", required = true)
     private int total_rating;
 
@@ -21,28 +28,33 @@ public class Driver extends User {
     private int total_passangers;
 
     @XmlElement(name = "location", required = true)
-    private String location;
+    private ArrayList<String> location;
 
     public Driver() {
 
     }
 
-    public Driver(int id, String name, String username, String email, String password, String phone_number, int driver, String image, int total_rating, int total_passangers, String location) {
-            super(id, name, username, email, password, phone_number, driver, image);
+    public Driver(int id, String name, int total_rating, int total_passangers, ArrayList<String> location) {
+            this.id = id;
+            this.name = name;
             this.total_rating = total_rating;
             this.total_passangers = total_passangers;
             this.location = location;
     }
+    
+    public String getName(){
+        return name;
+    }
 
     public int getTotalRating() {
-            return total_rating;
+        return total_rating;
     }
 
     public int getTotalPassangers() {
-            return total_passangers;
+        return total_passangers;
     }
 
-    public String getLocation() {
-            return location;
+    public ArrayList<String> getLocation() {
+        return location;
     }
 }
